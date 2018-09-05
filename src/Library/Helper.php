a489827010344e7253e946dbf699ac6e713361c3
crababy <727145108@qq.com>
2018-09-05 14:33:27 +0800

    update

--- src/Library/Helper.php
+++ src/Library/Helper.php
@@ -163,6 +163,8 @@ class Helper
 			$cip = $_SERVER["HTTP_X_FORWARDED_FOR"];
 		} else if (!empty($_SERVER["REMOTE_ADDR"])) {
 			$cip = $_SERVER["REMOTE_ADDR"];
+		} else if (!empty($_SERVER['HTTP_X_REAL_IP'])) {
+			$cip = $_SERVER["HTTP_X_REAL_IP"];
 		} else {
 			$cip = '';
 		}
